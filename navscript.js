7a8e2d2f9370c92fe83753c327820eb04fef10cc
Lilian Luong <lilianluong735@gmail.com>
2016-11-27 21:43:31 +0800

    bug fixes and layout

--- navscript.js
+++ navscript.js
@@ -3,39 +3,33 @@ $(document).ready(function(){
 	$("#contact-content").hide();
 	$("#pastSites-content").hide();
 	$("#home").click(function(){
+		console.log("home clicked");
 		$("#events-content").fadeOut("fast",function(){
 			$("#contact-content").fadeOut("fast",function(){
-				$("#pastSites-content").fadeOut("fast",function(){
+				$("#sidebar").fadeIn("fast", function(){
 					$("#home-content").fadeIn();
+					$("#content").css("width","calc(100% - 290px)");
 				});
-				
 			});
 		});
 	});
 	$("#events").click(function(){
+		console.log("events clicked");
 		$("#contact-content").fadeOut("fast",function(){
 			$("#home-content").fadeOut("fast",function(){
-				$("#pastSites-content").fadeOut("fast",function(){
+				$("#sidebar").fadeOut("fast",function(){
 					$("#events-content").fadeIn();
+					$("#content").css("width","100%");
 				});
-				
 			});
 		});
 	});
 	$("#contact").click(function(){
 		$("#events-content").fadeOut("fast",function(){
 			$("#home-content").fadeOut("fast",function(){
-				$("#pastSites-content").fadeOut("fast",function(){
+				$("#sidebar").fadeIn("fast", function(){
 					$("#contact-content").fadeIn();
-				});
-			});
-		});
-	});
-	$("#pastSites").click(function(){
-		$("#events-content").fadeOut("fast",function(){
-			$("#home-content").fadeOut("fast",function(){
-				$("#contact-content").fadeOut("fast",function(){
-					$("#pastSites-content").fadeIn();
+					$("#content").css("width","calc(100% - 290px)");
 				});
 			});
 		});
